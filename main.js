551986b4aab84b3d4f9234b09b16f56833d4f6bb
Deinonzch <slagol@st.amu.edu.pl>
2016-04-20 17:59:13 +0200

    prosty search

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-var r;
-var e;
+var app = angular.module('app', []);
 
-function odbierzDane()
-{
-    if (r.readyState == 4) {
-        if (r.status == 200 || r.status == 304) {
-            e.innerHTML = r.responseText;
-        }
-    }
-}
+app.controller('drinkiCTRL', function ($scope, $http) {
+    var app = this;
+    app.drinki = [
+        {nazwa: 'Adios Motherfucker (AMF) ', skadniki: 'wodka (15ml), rum (15ml), tequila (15ml), gin (15ml), ' +
+        'likier blue curacao (15 ml), sweet and sour mix (60 ml), sprite lub 7-Up (60 ml), kostki lodu',
+            przygotowanie: 'dodac kostki lodu do szklanki, wlac skladniki do szklanki highball, delikatnie wymieszac'},
+        {nazwa: 'Black Magic', skadniki: 'wodka (50 ml),likier kawowy (50 ml), sok z cytryny (kilka kropel), kostki lodu',
+            przygotowanie: 'dodac kostki lodu do szklanki, wlac skladniki do szklanki, wymieszac'},
+        {nazwa: 'Sonic Screwdriver', skadniki: 'wodka (40ml, likier blue curacao (40ml), sok pomaranczowy (240ml), kostki lodu',
+            przygotowanie: 'dodac kostki lodu do szklanki, wlac skladniki do szklanki, wymieszac'},
+        {nazwa: 'Kamikaze (Kamikadze)', skadniki: 'wodka (30 ml), triple sec (30 ml), sok z limonki (30 ml), kostki lodu',
+            przygotowanie: 'dodac kostki lodu do shakera, wlac skladniki do shakera, wstrzasnac, przez sitko wlac do kieliszka'},
+        {nazwa: 'Niebieskie Kamikadze (Blue Kamikaze)', skadniki: 'wodka (50 ml), likier blue curacao (50 ml), sok z cytryny (50 ml), kostki lodu',
+            przygotowanie: 'dodac kostki lodu do shakera, wlac skladniki do shakera, wstrzasnac, przez sitko wlac do kieliszka'},
+        {nazwa: 'Tequila Sunrise', skadniki: '    tequila (50 ml), sok pomaranczowy (150 ml), grenadina (25 ml), kostki lodu',
+            przygotowanie: 'dodac kostki lodu do polowy szklanki, wlac tequila i sok pomoranczowy do szklanki, wlac po woli grendina do szklanki'}
 
-function wymienTresc(adresurl, htmlid)
-{
-    if (r = getXMLHttpRequest()) {
-        e = document.getElementById(htmlid);
-        r.open('GET', adresurl);
-        r.onreadystatechange = odbierzDane;
-        r.send(null);
-    }
-}
+    ];
+
+});
